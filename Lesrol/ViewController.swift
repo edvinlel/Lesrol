14dae6a58bf0ff53727b6dd75f7364a7471c79fa
Edvin Lellhame <edvinlel@gmail.com>
2020-08-02 23:28:33 -0700

    Initial Commit

---
+++ Lesrol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesrol
+//
+//  Created by Edvin Lellhame on 8/2/20.
+//  Copyright © 2020 Edvin Lellhame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
